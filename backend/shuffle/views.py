1e11ddba19241802e4fcb8c12ffaf553745d0ef6
Yonas Marie~ <yonasgebre08@gmail.com>
2024-02-07 00:29:47 +0300

    integration

--- backend/shuffle/views.py
+++ backend/shuffle/views.py
@@ -6,6 +6,7 @@ from shuffle.functions import final_shuffle, python_shuffle
 # Create your views here.
 class Shuffler(APIView):
     def post(self, request):
+        print(request.data)
         input: List[any] = request.data["input"]
         response = {'data': final_shuffle(input)}
         return Response(response)
