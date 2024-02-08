262848e2942d8330e707549638beae9c09368c4c
Yonas Marie~ <yonasgebre08@gmail.com>
2024-02-08 22:31:22 +0300

    [frontend] build template

--- backend/shuffle/urls.py
+++ backend/shuffle/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from shuffle.views import Shuffler
+from shuffle.views import IndexView, Shuffler
 
 urlpatterns = [
-    path('', Shuffler.as_view()),
+    path('api/', Shuffler.as_view()),
+    path('', IndexView.as_view(), name='index'),
 ]
